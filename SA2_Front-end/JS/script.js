4c6ccf4cafbf535185019d42c391721b2f8316fd
Thiago Bazani <tgobzrj@live.com>
2022-02-07 19:41:51 -0300

    atualizacao

---
+++ SA2_Front-end/JS/script.js
@@ -0,0 +1,36 @@
+let novaNoticias = [
+  {
+    titulo: "Brasil é HexaCampeão do mundo",
+    descricao: "Contra a inglaterra brarsil leva a melhor fazendo 2 gols",
+  },
+  {
+    titulo: "CS-GO ganha update novo",
+    descricao: "Update de Fevereiro trás novidade ao jogoo como, mapa novo, uma nova arma e novas mecanicas",
+  },
+  {
+    titulo: "Brasil é HexaCampeão do mundo",
+    descricao: "Contra a inglaterra brarsil leva a melhor fazendo 2 gols",
+  },
+  {
+    titulo: "CS-GO ganha update novo",
+    descricao: "Update de Fevereiro trás novidade ao jogoo como, mapa novo, uma nova arma e novas mecanicas",
+  },
+  {
+    titulo: "Brasil é HexaCampeão do mundo",
+    descricao: "Contra a inglaterra brarsil leva a melhor fazendo 2 gols",
+  },
+  {
+    titulo: "CS-GO ganha update novo",
+    descricao: "Update de Fevereiro trás novidade ao jogoo como, mapa novo, uma nova arma e novas mecanicas",
+  },
+]
+
+function renderNoticias(){
+    let espaca = document.getElementById("noticias")
+    let template = ""
+
+    for (let i = 0; i < novaNoticias.length; i++) {
+      const noticias = novaNoticias[i];
+      noticias = innerHTML
+    }
+}
