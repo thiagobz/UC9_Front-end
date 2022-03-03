0a31462ff29c76e16ef7b3df271e79d222595e57
Thiago Bazani <tgobzrj@live.com>
2022-03-03 14:11:02 -0300

    Banner carrossel

--- SA2_Front-end/JS/script.js
+++ SA2_Front-end/JS/script.js
@@ -1,6 +1,30 @@
-$(document).ready(function () {
+$( document ).ready(function() {
 
- });
+  $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+
+  setInterval(slide, 1000)
+
+  function slide(){
+      if($(".banner-ativo").next().length ){
+          $(".banner-ativo").removeClass("banner-ativo").hide().next().addClass("banner-ativo").show()
+      }else{
+          $(".banner-ativo").removeClass().hide()
+          $("#carrosel img:eq(0)").addClass("banner-ativo").show()
+      }
+  }
+
+  $("#barras").click(function () {
+      $("#menu").toggleClass("menu-ativo")
+
+      // if ($("#menu").hasClass("menu-ativo")) {
+      //     $("#menu").removeClass("menu-ativo")
+      // } else {
+      //     $("#menu").addClass("menu-ativo")
+      // }
+  })
+
+  console.log(slide);
+})
 
 //Adicionar produtos semi-auto
 
